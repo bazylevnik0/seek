4db90d9ce607b9b2a85c29296a59009c47584b43
bazylevnik0 <bazylevnik0@gmail.com>
2022-11-23 17:10:47 +0300

    fix script

--- script.sh
+++ script.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 for i in {0..999999}
 do
-   python3 parser.py $((RANDOM % 255)).$((RANDOM % 255)).$((RANDOM % 255)).$((RANDOM % 255))
-   ./interpretator
+   if [ "$(ls -A ./output/)" ]; then
+     exit 0
+	else
+      python3 parser.py $((RANDOM % 255)).$((RANDOM % 255)).$((RANDOM % 255)).$((RANDOM % 255))
+     ./interpretator
+	fi
 done
