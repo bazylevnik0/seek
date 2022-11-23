9c817df006c09caab69e02b46122c856c33022d9
bazylevnik0 <bazylevnik0@gmail.com>
2022-11-23 18:08:35 +0300

    add msg to script

--- script.sh
+++ script.sh
@@ -2,9 +2,10 @@
 for i in {0..999999}
 do
    if [ "$(ls -A ./output/)" ]; then
+     echo "new image added to 'output', save and delete this image if you want launch again..."
      exit 0
 	else
       python3 parser.py $((RANDOM % 255)).$((RANDOM % 255)).$((RANDOM % 255)).$((RANDOM % 255))
-     ./interpretator
+      ./interpretator
 	fi
 done
